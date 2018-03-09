1a7c834dbec8d6afe5a70826504dc4768c3a87bc
bucong <531053266@qq.com>
2018-03-09 10:27:46 +0800

    更换云数据库

--- www/api/db.php
+++ www/api/db.php
@@ -1,5 +1,5 @@
 <?php
-$conn=@mysql_connect('localhost','root','') or die('连接数据库失败！');
+$conn=@mysql_connect('bucong1129.mysql.rds.aliyuncs.com','root','Bucong5733') or die('连接数据库失败！');
 mysql_select_db('lanbojini',$conn) or die('选择数据库失败！');
 mysql_query('set names utf8');
 ?>
